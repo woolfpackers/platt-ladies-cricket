2c07e8f804fecaf74cb8989a7ce2ee5ad30c74c1
Anthony Woolf <woolfpackers@gmail.com>
2026-04-11 02:08:58 +0100

    Changing text on About Our Club navigation button

--- lib/nav.ts
+++ lib/nav.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { href: '/', label: 'Home' },
-  { href: '/about', label: 'About our club' },
+  { href: '/about', label: 'Our club' },
   { href: '/squad', label: 'Squad' },
   { href: '/fixtures', label: 'Fixtures and Results' },
   { href: '/join-us', label: 'Join Us' },
