063e5a9ffb6ffcf4de5ae150d6032f813cf77f14
Anthony Woolf <woolfpackers@gmail.com>
2026-04-12 13:39:46 +0100

    fix fixtures page prop name

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -6,9 +6,10 @@ export const dynamic = 'force-dynamic';
 
 export default async function FixturesPage() {
   const fixtures = await getFixtures();
+
   return (
     <PageShell>
-      <FixturesResultsView fixtures={fixtures} />
+      <FixturesResultsView items={fixtures} />
     </PageShell>
   );
 }
