235b3d2c90af2bae69d8df05201012e13b2218b8
Anthony Woolf <woolfpackers@gmail.com>
2026-04-23 18:11:40 +0100

    Adding google search functionality

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+export default function sitemap() {
+  return [
+    { url: 'https://plattladiescricket.divelive.co.uk' },
+    { url: 'https://plattladiescricket.divelive.co.uk/about' },
+    { url: 'https://plattladiescricket.divelive.co.uk/squad' },
+    { url: 'https://plattladiescricket.divelive.co.uk/fixtures' },
+    { url: 'https://plattladiescricket.divelive.co.uk/join-us' },
+    { url: 'https://plattladiescricket.divelive.co.uk/tour' },
+    { url: 'https://plattladiescricket.divelive.co.uk/pavilion' },
+    { url: 'https://plattladiescricket.divelive.co.uk/sponsors' },
+    { url: 'https://plattladiescricket.divelive.co.uk/events' },
+    { url: 'https://plattladiescricket.divelive.co.uk/gallery' },
+    { url: 'https://plattladiescricket.divelive.co.uk/fundraising' },
+    { url: 'https://plattladiescricket.divelive.co.uk/partnership-opportunities' },
+    { url: 'https://plattladiescricket.divelive.co.uk/contact' },
+    
+  ];
+}
