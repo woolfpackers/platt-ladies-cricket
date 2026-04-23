8bf29131ab5b2c465d590a9c04ea536e593e74d5
Anthony Woolf <woolfpackers@gmail.com>
2026-04-23 16:43:36 +0100

    Updating image name for quiz

--- app/fundraising/page.tsx
+++ app/fundraising/page.tsx
@@ -52,7 +52,7 @@ export default function FundraisingPage() {
 	    style={{ display: 'block' }}
 	  >
 	    <img
-	      src="/images/quiz_night.png"
+	      src="/images/quiz_night.jpg"
 	      alt="Quiz Night"
 	      style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }}
 	    />
